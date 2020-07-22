dfe626a1ad66b30d2ab60fadf92b175544400185
Oleg Koryakov <outflowpwnz@gmail.com>
2020-07-22 11:32:08 +0700

    add eventemitter class

---
+++ src/ts/EventEmitter/EventEmitter.ts
@@ -0,0 +1,19 @@
+export default class EventEmitter implements IEventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  private events: IEvents;
+
+  on(eventName: string, callback: Function) {
+    if (this.events[eventName] === undefined) this.events[eventName] = [];
+    this.events[eventName].push(callback);
+  }
+
+  emit(eventName: string, arg: any) {
+    const events = this.events[eventName];
+    events.forEach((callbalck) => {
+      callbalck(arg);
+    });
+  }
+}
